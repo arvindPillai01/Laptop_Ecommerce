99887e43e95fc98bc5f4f8d57f45e47751e2dfe0
arvindPillai01 <Arvind Pillai@IN-LP-TR-16.sp.com>
2023-09-29 23:06:48 +0530

    adding screenshots and a background colour change

--- Laptop_Ecommerce/Controllers/HomeController.cs
+++ Laptop_Ecommerce/Controllers/HomeController.cs
@@ -19,12 +19,12 @@ namespace Laptop_Ecommerce.Controllers
             //return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
         public ActionResult Contact()
         {
